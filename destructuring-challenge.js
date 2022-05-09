cb5ac0406ae28434f3575f349a59682c8d98961b
SDDNG <davidangraham@gmail.com>
2022-05-09 10:06:44 +0000

    map and filter

---
+++ destructuring-challenge.js
@@ -0,0 +1,41 @@
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art', 'history', 'philosophy'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr,student) => {
+    for (let itm of arr){
+        if (itm.name == student) {
+            return itm.subjects;       
+        }
+    }
+}
+
+let [first, second, ...rest] = makeList(students, "John");
+console.log(first, second, rest);
+
+let [first1, second1, third1, ...rest1] = makeList(students, "Fran");
+console.log(first1, second1, third1, rest1);
+
